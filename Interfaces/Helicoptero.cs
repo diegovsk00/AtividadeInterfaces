0a710a7dee64261167ff65857b146c59243da7e5
diegovsk00 <diegolegal62@gmail.com>
2024-10-02 18:41:28 -0300

    Add files via upload
    ta aí

---
+++ Interfaces/Helicoptero.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Helicoptero : IMaquina, IPairador, IVoador
+    {
+        void IMaquina.Abastecer()
+        {
+            Console.WriteLine("Colocando combustivel no helicoptero");
+        }
+
+        void IMaquina.Alerta()
+        {
+            Console.WriteLine("Alerta a helice esta com poblema");
+        }
+
+        void IVoador.Cair()
+        {
+            Console.WriteLine("O helicoptero esta caindo");
+        }
+
+        void IVoador.Decolar()
+        {
+            Console.WriteLine("O helicoptero esta se preparando para decolar");
+        }
+
+        void IMaquina.Desligar()
+        {
+            Console.WriteLine("Deslligando helicoptero apos pouso");
+        }
+
+        bool IEquatable<IVoador>.Equals(IVoador? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMaquina.Ligar()
+        {
+            Console.WriteLine("Ligando o helicoptero para a decolagem");
+        }
+
+        void IPairador.Pairar()
+        {
+            Console.WriteLine("O helicoptero esta pairando sobre a cidade");
+        }
+
+        void IVoador.Planar()
+        {
+            Console.WriteLine("O helicoptero esta diminuindo a potencia das helices");
+        }
+
+        void IVoador.Pousar()
+        {
+            Console.WriteLine("O helicoptero esta se preparando para pousagem");
+        }
+    }
+}
---
+++ Interfaces/SuperMan.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class SuperMan : IVoador, IPairador, ISerVivo
+    {
+        void IVoador.Cair()
+        {
+            Console.WriteLine("Jogaram kriptonita e ele caiu");
+        }
+
+        void ISerVivo.Comer()
+        {
+            Console.WriteLine("Superman almoçando com sua mãe");
+        }
+
+        void IVoador.Decolar()
+        {
+            Console.WriteLine("Ele olha pro céu, respira fundo e sai do chão com uma vrlocidade imensa");
+        }
+
+        void ISerVivo.Dormir()
+        {
+            Console.WriteLine("Após um dia combatendo vilões, ele foi dormir cansado");
+        }
+
+        bool IEquatable<IVoador>.Equals(IVoador? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISerVivo.Morrer()
+        {
+            Console.WriteLine("Superman morre?");
+        }
+
+        void IPairador.Pairar()
+        {
+            Console.WriteLine("Superman observando a cidade lá do alto");
+        }
+
+        void IVoador.Planar()
+        {
+            Console.WriteLine("Ele cansa de voar rápido e voa mais tranquilamente");
+        }
+
+        void IVoador.Pousar()
+        {
+            Console.WriteLine("Superman vem do céu numa grande velocidade e quebra o chão");
+        }
+
+        void ISerVivo.Respirar()
+        {
+            Console.WriteLine("O cara precisa respirar pra viver ");
+        }
+    }
+}
